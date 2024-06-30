879a35a80738fcd347d00e7cdf9ea1d0d6f3130d
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2024-06-30 22:59:46 +0530

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  HandTracking
+//  Doodle
 //
-//  Created by Srujan on 2/23/24.
+//  Created by Pradyut Fogla on 26/02/24.
 //
 
 import SwiftUI
@@ -10,39 +10,12 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-    
-    @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
-    
-    @Environment(\.openImmersiveSpace) var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
-    
     var body: some View {
-        VStack {
-            Text("Hand Tracking")
-            
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
-        }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
-                }
-            }
-        }
+       PanelSelectionView()
     }
+}
+
+#Preview {
+    ContentView()
+        .environment(ViewModel())
 }
