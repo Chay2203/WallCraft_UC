dd7dd3c543dd626de180078da81801416e3d671d
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2024-06-30 23:40:58 +0530

    Add files via upload

--- ImmersiveView.swift
+++ ImmersiveView.swift
@@ -1,27 +1,86 @@
+//
+//  ImmersiveView.swift
+//  Doodle
+//
+//  Created by Pradyut Fogla on 26/02/24.
+//
+
 import SwiftUI
 import RealityKit
-import ARKit
+import RealityKitContent
 
 struct ImmersiveView: View {
-    // @State property wrapper to manage the state of plane detection within the SwiftUI view
-    @State var planeDetection = PlaneDetectionModel()
+    @Environment(ViewModel.self) private var model
+    @State private var planeEntity: Entity?
     
     var body: some View {
-        // RealityView is a custom view that integrates with RealityKit for AR experiences
+        @Bindable var model = model
         RealityView { content in
-            // Adds the rootEntity of the planeDetection model to the RealityView's content
-            // This is where the detected planes or other AR content would be added
-            content.add(planeDetection.rootEntity)
+            if let planeEntity = planeEntity {
+                            content.add(planeEntity)
+                        }
+                    }
+                    .onAppear {
+                        updatePlaneEntity() // Initial setup
+                    }
+                    .onChange(of: model.flowState) { _,_ in
+                        updatePlaneEntity() 
+                        print("updating img")// Update when flowState changes
+                    }
+        
+        
         }
-        .task {
-            // Asynchronously starts the AR session with plane detection
-            // This method is expected to configure and run the AR session
-            await planeDetection.startSession()
-        }
-        .onDisappear {
-            // Stops the AR session when the view disappears
-            // This is important for freeing up resources and stopping the AR experience when not needed
-            planeDetection.stopSession()
+       
+    
+    private func updatePlaneEntity(){
+        let wallAnchor = AnchorEntity(.plane(.vertical, classification: .wall, minimumBounds: SIMD2<Float>(1.0, 0.2)))
+        
+        let planeMesh = MeshResource.generatePlane(width: 0.70, depth: 4.775, cornerRadius: 0.0)
+        
+        let img = Image("panel.png")
+            .resizable(resizingMode: .tile)
+        
+        let material: SimpleMaterial
+                switch model.flowState {
+                case .one:
+                    material = ImmersiveView.loadImageMaterial(imageUrl: "panel.png")
+                case .two:
+                    material = ImmersiveView.loadImageMaterial(imageUrl: "wally.png")
+                case .three:
+                    material = ImmersiveView.loadImageMaterial(imageUrl: "panel.png")
+                }
+    
+        
+        let planeEntity = ModelEntity(mesh: planeMesh, materials: [material])
+        
+           planeEntity.name = "canvas"
+           wallAnchor.addChild(planeEntity)
+        
+        self.planeEntity = wallAnchor
+       }
+    
+
+    
+    
+    static func loadImageMaterial(imageUrl: String) -> SimpleMaterial {
+            do {
+                let texture = try TextureResource.load(named: imageUrl)
+                
+            
+                
+                var material = SimpleMaterial()
+                material.baseColor = MaterialColorParameter.texture(texture)
+                
+                
+                return material
+            } catch {
+                fatalError(String(describing: error))
+            }
         }
     }
+//UNABle to see pictures after one full scroll
+
+#Preview {
+    ImmersiveView()
+        .previewLayout(.sizeThatFits)
 }
